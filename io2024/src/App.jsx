123cfc691627419d7eba76a3829aa6d673e7ee9a
Czechu7 <choocas0@gmail.com>
2023-11-29 02:14:03 +0100

    mainpage+recepcja
    mainpage+recepcja

--- io2024/src/App.jsx
+++ io2024/src/App.jsx
@@ -5,16 +5,20 @@ import Mechanik from './components/Mechanik.jsx'
 import Magazyn from './components/Magazyn.jsx'
 import Kierownik from './components/Kierownik.jsx'
 import Recepcja from './components/Recepcja.jsx'
+import LoginPage from './components/auth/LoginPage.jsx';
+import RejestrKlientowRecepcja from './components/recepcja/RejestrKlientowRecepcja.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<MainPage/>} />
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/home' element={<MainPage/>} />
         <Route path='/mechanik' element={<Mechanik/>} />
         <Route path='/magazyn' element={<Magazyn/>} />
         <Route path='/kierownik' element={<Kierownik/>} />
         <Route path='/recepcja' element={<Recepcja/>} />
+        <Route path='/rejestrklientowrecepcja' element={<RejestrKlientowRecepcja/>} />
       </Routes>
     </Router>
   );
