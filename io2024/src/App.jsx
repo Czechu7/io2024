1f5a2fc3fdeeee11cda3dda1ece8d30b44cf446a
Evivalarte <piotr.sadowski.ps@icloud.com>
2023-11-29 02:24:57 +0100

    Warsztat component
    Warsztat component

--- io2024/src/App.jsx
+++ io2024/src/App.jsx
@@ -5,6 +5,8 @@ import Mechanik from './components/Mechanik.jsx'
 import Magazyn from './components/Magazyn.jsx'
 import Kierownik from './components/Kierownik.jsx'
 import Recepcja from './components/Recepcja.jsx'
+import RejestrZlecenMechanik from './components/mechanik/RejestrZlecenMechanik.jsx';
+import KosztorysMechanik from './components/mechanik/KosztorysMechanik.jsx';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path='/magazyn' element={<Magazyn/>} />
         <Route path='/kierownik' element={<Kierownik/>} />
         <Route path='/recepcja' element={<Recepcja/>} />
+        <Route path='/rejestrzlecenmechanik' element={<RejestrZlecenMechanik/>} />
+        <Route path='/kosztorysmechanik' element={<KosztorysMechanik/>} />
+
       </Routes>
     </Router>
   );
---
+++ io2024/src/components/mechanik/KosztorysMechanik.jsx
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import '../../css/mechanik/KosztorysMechanik.css'
+
+
+function KosztorysMechanik() {
+  return (
+    <div className="KosztorysMechanik">
+      <h1>KosztorysMechanik</h1>
+<input type="text" id="nazwa-input" placeholder="Wprowadź nazwę części" />
+<input type="text" className='textarea' id="nazwa-input" placeholder="Wprowadź ilość roboczogodzin" />
+<button className='btnm' id="przeslij-button">Prześlij</button>
+      <Button className='btnm' type="submit"  href="/" >Powrót</Button>{' '}
+    </div>
+  );
+}
+
+export default KosztorysMechanik
