22a4f6e8add328a8ad3865edc746465c7f6cbf75
Czechu7 <choocas0@gmail.com>
2024-01-09 00:07:14 +0100

    letsgo

--- io2024/src/components/Kierownik.jsx
+++ io2024/src/components/Kierownik.jsx
@@ -8,30 +8,21 @@ function Kierownik() {
 	return (
 		//początek
 		<div>
-			<div className="Kierownik">
 				<KierownikHeader></KierownikHeader>
+				<div className="MainPage">
+			<div className="ButtonContainer">
+						<Button className="Button" type="submit" href="rejestrzlecenkierownik">Rejestr zleceń
+				</Button>
+
+				<Button className="Button" type="submit" href="rejestrpracownikow">
+				Rejestr pracowników
+				</Button>
+
+				<Button className="Button" type="submit" href="rejestrfinansow">
+				Rejestr finansów
+				</Button>
+				</div>
 			</div>
-			<Link to={"/rejestrzlecenkierownik"}>
-				<div>
-					<Button type="button">Rejestr zleceń</Button>
-				</div>
-			</Link>
-			<Link to={"/rejestrpracownikow"}>
-				<div>
-					<Button type="button">Rejestr pracowników</Button>
-				</div>
-			</Link>
-			<Link to={"/rejestrfinansow"}>
-				<div>
-					<Button type="button">Rejestr finansów</Button>
-				</div>
-			</Link>
-			<br />
-			<Link to={"/home"}>
-				<div>
-					<Button type="button">Powrót do strony głównej</Button>
-				</div>
-			</Link>
 		</div>
 	);
 }
--- io2024/src/components/Mechanik.jsx
+++ io2024/src/components/Mechanik.jsx
@@ -3,13 +3,19 @@ import Button from 'react-bootstrap/Button';
 
 function Mechanik() {
   return (
-    <div>
-      <h1>Mechanik</h1>
-    <div><Button type="submit"  href="/rejestrzlecen" >Rejestr zleceń</Button></div>
-    <div><Button type="submit"  href="/kosztorysmechanik" >Kosztorys</Button></div>
-    <div><Button type="submit"  href="/home" >Strona główna</Button></div>
+  <div className="MainPage">
+  <div className="ButtonContainer">
+        <Button className="Button" type="submit" href="rejestrzlecen">Rejestr zleceń
+    </Button>
+
+    <Button className="Button" type="submit" href="kosztorysmechanik">
+    Kosztorys
+    </Button>
+
 
     </div>
+  </div>
+
   );
 }
 
